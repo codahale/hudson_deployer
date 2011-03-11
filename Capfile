d2be01aad6260cc679dd677b592e4116ccfd2e9b
Collin VanDyck <cvandyck@yammer-inc.com>
2011-03-11 14:07:16 -0800

    First commit

---
+++ Capfile
@@ -0,0 +1,7 @@
+require 'deployer'
+
+set :hudson, "build.yammer.com"
+set :application, "observatory-release"
+set :version, "2.0.0"
+set :user, "cvandyck"
+
---
+++ deployer.rb
@@ -0,0 +1,105 @@
+require 'capistrano'
+require 'rest_client'
+require 'json'
+
+class Deploy
+  attr_accessor :application, 
+                :version, 
+                :user, 
+                :build_num, 
+                :artifact_url
+                
+  def initialize(&block)
+    if block_given?
+      yield self
+    end
+  end
+  
+  def debug
+    puts "Deployment Configuration:"
+    [:application, :version, :user, :build_num, :artifact_url].each do |f|
+      puts "  #{f}: #{send(f)}"
+    end
+  end
+  
+end
+
+Capistrano::Configuration.instance(:must_exist).load do
+
+  def _cset(name, *args, &block)
+    unless exists?(name)
+      set(name, *args, &block)
+    end
+  end
+
+  _cset(:hudson) { abort ":hudson must be set (e.g. build.yammer.com)" }
+  _cset(:application) { abort ":application must be set" }
+  _cset(:version) { abort ":version must be set (e.g. 2.0.0)" }
+  _cset(:user) { abort ":user must be set" }
+
+  def hud_api(url)
+    JSON.parse(RestClient.get(url + "/api/json"))
+  end
+
+  def job
+    @job ||= begin
+      record = hud_api(hudson)["jobs"].find { |j| j["name"] == application }
+      raise "No such hudson build could be found" if !record
+      hud_api(record["url"])
+    end
+  end
+  
+  def latest_build
+    @latest_build ||= begin
+      builds = job["builds"]
+      raise "There are no existing builds for this project" if builds.empty?
+      url = builds.first["url"]
+      hud_api(url)
+    end
+  end
+  
+  def check_latest_build
+    if latest_build["result"] != "SUCCESS"
+      raise "The last build was not successful: #{build["result"]}"
+    end
+    if latest_build["building"] == true
+      raise "Project is currently building. You must wait until it finishes."
+    end
+  end
+  
+  def resolve_artifact_url
+    artifacts = latest_build["artifacts"]
+    raise "No artifacts exist for this project!" if artifacts.empty?
+    artifact = if artifacts.length > 1
+      puts "More than one artifact was found. Please choose:"
+      artifacts.each_with_index do |artifact, index|
+        puts "#{index}: #{artifact["relativePath"]}"
+      end
+      print  "? "
+      artifact_index = Capistrano::CLI.ui.ask("choice").to_i
+      artifacts[artifact_index]
+    else
+      artifacts.first
+    end
+    artifact["relativePath"]
+  end
+  
+  def build_deployment
+    check_latest_build
+    Deploy.new do |d|
+      d.application = application
+      d.version = version
+      d.user = user
+      d.build_num = latest_build["number"]
+      d.artifact_url = resolve_artifact_url
+    end
+  end
+
+  task "deploy" do
+    @deploy = build_deployment
+    @deploy.debug
+    
+  end
+
+end
+
